c9ab2692a5fde8cf6c79a42909953890d7a08b6d
Shian Su <su.s@mac3001.wehi.edu.au>
2017-03-22 15:38:31 +1100

    Added single/paired guessing

--- geo_helper.R
+++ geo_helper.R
@@ -1,8 +1,7 @@
-str_detect <- function(x, pattern) {
-    grepl(pattern, x)
-}
-
 get_raw_files_info <- function() {
+    str_detect <- function(x, pattern) {
+        grepl(pattern, x)
+    }
 
     get_fastq_files <- function() {
         filter <- function(x, f) {
@@ -10,16 +9,6 @@ get_raw_files_info <- function() {
         }
 
         filter(dir(), function(x) str_detect(x, ".fastq"))
-    }
-
-    guess_read_len <- function(x) {
-        mode <- function(x) {
-            tab <- table(x)
-            names(tab)[which(tab == max(tab))]
-        }
-
-        reads <- get_reads(x, n = 200)
-        mode(nchar(reads))
     }
 
     get_reads <- function(x, n = 100) {
@@ -36,15 +25,45 @@ get_raw_files_info <- function() {
         output[(1:n)*4 - 2]
     }
 
+    guess_read_len <- function(x) {
+        mode <- function(x) {
+            tab <- table(x)
+            names(tab)[which(tab == max(tab))]
+        }
+
+        reads <- get_reads(x, n = 200)
+        mode(nchar(reads))
+    }
+
+    guess_paired_status <- function(x) {
+        guess_func <- function(fname) {
+            if (grepl("R1.fastq", fname)) {
+                partner <- gsub("R1.fastq", "R2.fastq", fname)
+                if (partner %in% x) {
+                    return("paired")
+                }
+            } else if (grepl("R2.fastq", fname)) {
+                partner <- gsub("R2.fastq", "R1.fastq", fname)
+                if (partner %in% x) {
+                    return("paired")
+                }
+            }
+            return("single")
+        }
+        sapply(x, guess_func)
+    }
+
     fastq_files <- get_fastq_files()
     read_lengths <- sapply(fastq_files, guess_read_len)
     md5sums <- tools::md5sum(fastq_files)
+    paired_status <- guess_paired_status(fastq_files)
 
     df <- data.frame(file.name = fastq_files,
                      file.type = "fastq",
                      file.checksum = md5sums,
                      instrument = " ",
-                     read.length = read_lengths)
+                     read.length = read_lengths,
+                     paired.status = paired_status)
 
     paste(apply(df, 1, function(x) paste(x, collapse = "\t")), collapse = "\n")
 }
