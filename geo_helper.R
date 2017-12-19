059011e08ba424cd4eb17678ee8e41ac4a837d2c
Shians <registertonysu@gmail.com>
2017-12-19 18:09:04 +1100

    Implemented instrument guessing

--- geo_helper.R
+++ geo_helper.R
@@ -1,60 +1,106 @@
-get_raw_files_info <- function() {
-    str_detect <- function(x, pattern) {
-        grepl(pattern, x)
+str_detect <- function(x, pattern) {
+    grepl(pattern, x)
+}
+
+get_fastq_files <- function() {
+    filter <- function(x, f) {
+        x[f(x)]
     }
 
-    get_fastq_files <- function() {
-        filter <- function(x, f) {
-            x[f(x)]
-        }
+    filter(dir(), function(x) str_detect(x, ".fastq"))
+}
 
-        filter(dir(), function(x) str_detect(x, ".fastq"))
-    }
+get_reads <- function(x, n = 200) {
+    output <- readLines(x, n = n * 4)
 
-    get_reads <- function(x, n = 200) {
-        output <- readLines(x, n = n * 4)
+    output[(1:n)*4 - 2]
+}
 
-        output[(1:n)*4 - 2]
-    }
+# instrument information taken from https://github.com/10XGenomics/supernova/blob/master/tenkit/lib/python/tenkit/illumina_instrument.py#L12-L45
+get_instrument <- function(x) {
+    guess_instrument <- function(x) {
+        instruments_df <- data.frame(
+            rbind(
+                c("HWI-M[0-9]{4}$", "MiSeq"),
+                c("HWUSI", "Genome Analyzer IIx"),
+                c("M[0-9]{5}$", "MiSeq"),
+                c("HWI-C[0-9]{5}$", "HiSeq 1500"),
+                c("C[0-9]{5}$", "HiSeq 1500"),
+                c("HWI-D[0-9]{5}$", "HiSeq 2500"),
+                c("D[0-9]{5}$", "HiSeq 2500"),
+                c("J[0-9]{5}$", "HiSeq 3000"),
+                c("K[0-9]{5}$", "HiSeq 3000 or HiSeq 4000"),
+                c("E[0-9]{5}$", "HiSeq X"),
+                c("NB[0-9]{6}$", "NextSeq"),
+                c("NS[0-9]{6}$", "NextSeq"),
+                c("MN[0-9]{5}$", "MiniSeq")
+            )
+        )
+        colnames(instruments_df) <- c("pattern", "instrument")
 
-    guess_read_len <- function(x) {
-        mode <- function(x) {
-            tab <- table(x)
-            names(tab)[which(tab == max(tab))]
-        }
-
-        reads <- get_reads(x, n = 200)
-        mode(nchar(reads))
-    }
-
-    guess_paired_status <- function(x) {
-        guess_func <- function(fname) {
-            if (str_detect(fname, "R1.fastq")) {
-                partner <- gsub("R1.fastq", "R2.fastq", fname)
-                if (partner %in% x) {
-                    return("paired")
-                }
-            } else if (str_detect(fname, "R2.fastq")) {
-                partner <- gsub("R2.fastq", "R1.fastq", fname)
-                if (partner %in% x) {
-                    return("paired")
-                }
+        for (i in seq_len(nrow(instruments_df))) {
+            if (grepl(instruments_df$pattern[i], x)) {
+                return(as.character(instruments_df$instrument[i]))
             }
-            return("single")
         }
-        sapply(x, guess_func)
+
+        return("Unknown")
     }
 
+
+    header <- get_reads(x, 1)[1]
+
+    if (header[1] != "@") {
+        return("Unknow")
+    }
+
+    fields <- unlist(strsplit(header, ":"))
+    instrument_id <- fields[1]
+    flowcell_id <- fields[3]
+
+    guess_instrument(instrument_id)
+}
+
+guess_read_len <- function(x) {
+    mode <- function(x) {
+        tab <- table(x)
+        names(tab)[which(tab == max(tab))]
+    }
+
+    reads <- get_reads(x, n = 200)
+    mode(nchar(reads))
+}
+
+guess_paired_status <- function(x) {
+    guess_func <- function(fname) {
+        if (str_detect(fname, "R1.fastq")) {
+            partner <- gsub("R1.fastq", "R2.fastq", fname)
+            if (partner %in% x) {
+                return("paired")
+            }
+        } else if (str_detect(fname, "R2.fastq")) {
+            partner <- gsub("R2.fastq", "R1.fastq", fname)
+            if (partner %in% x) {
+                return("paired")
+            }
+        }
+        return("single")
+    }
+    sapply(x, guess_func)
+}
+
+get_raw_files_info <- function() {
     fastq_files <- get_fastq_files()
-    read_lengths <- sapply(fastq_files, guess_read_len)
     md5sums <- tools::md5sum(fastq_files)
+    instrument <- sapply(fastq_files, get_instrument)
+    read_lengths <- sapply(fastq_files, guess_read_len)
     paired_status <- guess_paired_status(fastq_files)
 
     df <- data.frame(
         file.name = fastq_files,
         file.type = "fastq",
         file.checksum = md5sums,
-        instrument = " ",
+        instrument = instrument,
         read.length = read_lengths,
         paired.status = paired_status
     )
